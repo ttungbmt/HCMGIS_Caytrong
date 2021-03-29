607f111726d4629105398eaca4e233583a664ed4
Truong Thanh Tung <ttungbmt@gmail.com>
2021-03-29 14:54:18 +0700

    Update code

--- backend-app/app/Providers/NovaServiceProvider.php
+++ backend-app/app/Providers/NovaServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\User;
+use App\Nova\Layouts\KdThuocBVTVStats;
 use App\Nova\Metrics\Caytrongs;
 use App\Nova\Metrics\NewThuadat;
 use App\Nova\Metrics\NewUsers;
@@ -29,17 +30,11 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
 
         NovaPage::setConfig([
             'icon' => '<svg class="sidebar-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" /> </svg>',
-            'navTitle' => __('Statistics')
+            'navTitle' => 'Statistics'
         ]);
 
         NovaPage::addLayout(NonghoStats::class);
         NovaPage::addLayout(KdThuocBVTVStats::class);
-
-//        NovaPage::addPageFields('stats/kt-thuoc-bvtv', function (){
-//            return [
-//                Text::make('Name')
-//            ];
-//        });
 
         NovaMap::setConfig(function (){
             $layers = [
